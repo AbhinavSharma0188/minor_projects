d506f0e8ab9743944df271949301d15e97254f5e
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-01-26 20:11:42 +0530

    user model created

---
+++ backend/model/Usermodel.js
@@ -0,0 +1,14 @@
+import mongoose, { mongo } from "mongoose";
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+
+},{timestamps:true});
+const User=mongoose.model("User",userSchema);
